b8a1b3c92348c641ea54e3fe8ef2a7d5d79be4ba
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-15 23:43:58 +0530

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from "../img/logo.png"
 
 const Header = () => {
   return (
@@ -6,7 +7,12 @@ const Header = () => {
       <div className="w-screen fixed z50 p-6 px-16">
         {/* desktop view */}
         <div className="hidden md:flex w-full h-full ">
-            
+            <div className="flex items-center gap-2">
+              <img src={Logo} className="w-10 object-cover" alt="logo" />
+              <p className="text-headingColor text-xl font-bold">
+                Chick
+              </p>
+            </div>
         </div>
 
         {/* mobile view */}
