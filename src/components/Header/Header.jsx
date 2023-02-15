03f6657732bfb9fdc56da1dbbab569d69c9079db
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-15 23:37:23 +0530

    img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      Header
+      <div className="w-screen fixed z50 p-6 px-16">
+        {/* desktop view */}
+        <div className="hidden md:flex w-full h-full ">
+            
+        </div>
+
+        {/* mobile view */}
+        <div className="flex md:hidden w-full h-full ">
+
+        </div>
+      </div>
     </div>
   )
 }
